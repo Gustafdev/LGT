8530d47eee1d2cbc84a30a0a9624434562dd3433
mrryyi <jrhagelin@gmail.com>
2022-10-11 20:20:27 +0200

    Consequent standards; improved increment of loops

--- LGT.cpp
+++ LGT.cpp
@@ -8,13 +8,13 @@ int main()
 	int randno;
 	
 	srand(time(0));
-	int r, rader, kolumner;
+	int rader, kolumner;
 	cout << "Skriv in antal rader och kolumner: ";
 	cin >> rader >> kolumner;
 
-	for (r = 1; r <= rader; r++)
+	for (int r = 1; r <= rader; ++r)
 	{
-		for (int i = 0; i <kolumner; i++)
+		for (int i = 1; i <= kolumner; ++i)
 		{
 
 			randno = rand() % 14;
